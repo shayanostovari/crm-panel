9bf4d5ec70173520ef60ff95d4635b61dfa2b352
shayanostovari <ostshayan@gmail.com>
2025-10-12 13:19:23 +0200

    change

--- invoice/views.py
+++ invoice/views.py
@@ -1,16 +1,16 @@
+# invoice/views.py
 from django.views.generic import DetailView
 from django_weasyprint import WeasyTemplateResponseMixin
 from django.http import HttpResponse
 import traceback
 from .models import Invoice
 
-# ---- نمایش فایل PDF فاکتور ----
+
 class InvoicePDFView(WeasyTemplateResponseMixin, DetailView):
     model = Invoice
     template_name = 'invoice/invoice_template.html'
     pdf_filename = 'invoice.pdf'
 
-    # فیلدهای context در قالب HTML
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         obj = self.get_object()
@@ -18,19 +18,24 @@ class InvoicePDFView(WeasyTemplateResponseMixin, DetailView):
         context.update({
             'invoice': obj,
             'object': obj,
+            # اطلاعات ثابت شرکت
             'company_name': 'کانون آگهی و تبلیغات بازارسازان هوشمند پویا',
             'company_address': 'تهران، کیلومتر ۱۴ جاده مخصوص کرج...',
             'company_phone': '۴۴۷۸۲۳۵۱ - ۴۴۷۸۰۱۵۶',
-            'customer_name': obj.customer_name,
-            'total_price': obj.total_price,
+            # فیلدهای واقعی مدل فاکتور
+            'business_name': obj.business_name,
+            'agency_manager': obj.agency_manager,
+            'total_price': obj.total_amount(),  # تابع محاسبه مجموع کل از مدل
         })
         return context
 
-    # هندل خطاها برای مشاهده‌ی Traceback در صورت ارور 500
     def get(self, request, *args, **kwargs):
+        """هندل خطاها برای مشاهده‌ی traceback در صورت ارور ۵۰۰"""
         try:
             return super().get(request, *args, **kwargs)
         except Exception:
             return HttpResponse(
-                traceback.format_exc(), content_type="text/plain", status=500
+                traceback.format_exc(),
+                content_type="text/plain",
+                status=500
             )
